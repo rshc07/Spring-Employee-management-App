298e15d061f3be575f80117b7afcbb63493e89e6
HEMACHANDRAN RS <rshc07@gmail.com>
2024-07-27 20:19:47 +0530

    javax presistence modified

--- src/main/java/com/guvi/employeemanagement/model/Employee.java
+++ src/main/java/com/guvi/employeemanagement/model/Employee.java
@@ -1,10 +1,11 @@
 package com.guvi.employeemanagement.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
